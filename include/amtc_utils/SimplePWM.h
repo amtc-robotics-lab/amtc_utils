dca0205c7903faf180feb4b3e6049814e2dc6f3b
Felipe Inostroza <finostro@ug.uchile.cl>
2023-10-02 23:30:59 -0300

    FI: utils port in progress

--- include/amtc_utils/SimplePWM.h
+++ include/amtc_utils/SimplePWM.h
@@ -9,8 +9,7 @@
 #ifndef AMTC_UTILS_SIMPLEPWM_H
 #define AMTC_UTILS_SIMPLEPWM_H
 
-#include <ros/ros.h>
-#include <amtc_common/Debug.h>
+#include <rclcpp/rclcpp.hpp>
 
 #include <string>
 
@@ -33,13 +32,13 @@ public:
   bool set_period(double period)
   {
     _period = period;
-    _init_time = ros::Time::now();
+    _init_time = rclcpp::Time::now();
 
     if( _period < 0.01)
     {
       _period = 0.01;
 
-      ROS_ERROR("SimplePWM: period < 0.01, set as 0.01");
+      RCLCPP_ERROR("SimplePWM: period < 0.01, set as 0.01");
       set_duty_cycle(_duty_cycle);
 
       return false;
@@ -59,7 +58,7 @@ public:
     {
       _duty_cycle = 0.0;
       _calculed_duty_cycle = 0.0;
-      ROS_ERROR("SimplePWM: duty_cycle < 0.0, set as 0.0");
+      RCLCPP_ERROR("SimplePWM: duty_cycle < 0.0, set as 0.0");
 
       return false;
     }
@@ -68,7 +67,7 @@ public:
     {
       _duty_cycle = 1.0;
       _calculed_duty_cycle = _period;
-      ROS_ERROR("SimplePWM: duty_cycle > 1.0, set as 1.0");
+      RCLCPP_ERROR("SimplePWM: duty_cycle > 1.0, set as 1.0");
 
       return false;
     }
@@ -83,7 +82,7 @@ public:
 
   void start()
   {
-    _init_time = ros::Time::now();
+    _init_time = rclcpp::Time::now();
     _is_running = true;
   }
 
@@ -101,7 +100,7 @@ public:
   {
     if(_is_running == true)
     {
-      return (unsigned int)std::floor(((ros::Time::now() - _init_time).toSec())/_period);
+      return (unsigned int)std::floor(((rclcpp::Time::now() - _init_time).toSec())/_period);
     }
 
     return  0;
@@ -127,7 +126,7 @@ public:
         return true;
       }
 
-      return (std::fmod(((ros::Time::now() - _init_time).toSec()), _period)> _calculed_duty_cycle) ? false : true;
+      return (std::fmod(((rclcpp::Time::now() - _init_time).toSec()), _period)> _calculed_duty_cycle) ? false : true;
     }
 
     return false;
@@ -135,7 +134,7 @@ public:
 
 private:
 
-  ros::Time _init_time;
+  rclcpp::Time _init_time;
   double _duty_cycle;
   double _calculed_duty_cycle;
   double _period;
--- include/amtc_utils/resources/TopicResource.h
+++ include/amtc_utils/resources/TopicResource.h
@@ -61,7 +61,7 @@ protected:
   rclcpp::Node::SharedPtr node_;
   rclcpp::Subscription<T>::SharedPtr subscription_;
 
-  ros::Time last_update_time_;
+  rclcpp::Time last_update_time_;
   ros::Duration time_out_duration_;
   bool new_data_;
   typename T::ConstPtr data_;
@@ -94,7 +94,7 @@ protected:
 
   void msg_cb(const typename T::ConstPtr& msg){
     data_ = msg;
-    last_update_time_ = ros::Time::now();
+    last_update_time_ = rclcpp::Time::now();
     new_data_         = true;
   }
 
@@ -104,7 +104,7 @@ protected:
 
   inline bool is_available()
   {
-    bool timed_out = ros::Time::now() - last_update_time_ > time_out_duration_;
+    bool timed_out = rclcpp::Time::now() - last_update_time_ > time_out_duration_;
     return data_ && !timed_out;
   }
 
@@ -123,13 +123,13 @@ protected:
     {
       if(!is_available())
       {
-        ROS_WARN_NODE_THROTTLE(1.0,"Attempting to read %s message on topic %s but data is timed out", ros::message_traits::DataType<T>::value(), topic_name_.c_str());
+        RCLCPP_WARN_NODE_THROTTLE(1.0,"Attempting to read %s message on topic %s but data is timed out", ros::message_traits::DataType<T>::value(), topic_name_.c_str());
         return default_data_;
       }
       return *data_;
     }
       
-    ROS_WARN_NODE_THROTTLE(5.0, "Reading message on topic %s before any topic is received, returning default",topic_name_.c_str());
+    RCLCPP_WARN_NODE_THROTTLE(5.0, "Reading message on topic %s before any topic is received, returning default",topic_name_.c_str());
     
     return default_data_;
   }
